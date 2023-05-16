14a035a230d31214a690ac30ff7f889a5f9b7909
hagiape <g_chrisf@protonmail.com>
2023-05-16 20:31:23 +0800

    changed order of parameters

--- TV.py
+++ TV.py
@@ -1,9 +1,9 @@
 class TV:
-    def __init__(self, brand='Brand X', channel=20, volume_level=3, on=True):
-        self.brand = brand
+    def __init__(self, channel=20, volume_level=3, on=True, brand='Brand X'):
         self.channel = channel
         self.volume_level = volume_level
         self.on = on
+        self.brand = brand
     def turn_off(self):
         self.on = False
     def turn_on(self):
