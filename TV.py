29796ff8c8d8fc82a04056b24ee216f96b8a3459
hagiape <g_chrisf@protonmail.com>
2023-05-16 19:14:07 +0800

    defined __init__ constructor for the default TV

---
+++ TV.py
@@ -0,0 +1,6 @@
+class TV:
+    def __init__(self, brand='Brand X', channel='20', volume_level=3, on=True):
+        self.brand = brand
+        self.channel = channel
+        self.volume_level = volume_level
+        self.on = on
