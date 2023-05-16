8defd0b567ce90b9c1e6edcb31013222b3697cc8
hagiape <g_chrisf@protonmail.com>
2023-05-16 20:32:32 +0800

    changed default value for the power status of TV

--- TV.py
+++ TV.py
@@ -1,5 +1,5 @@
 class TV:
-    def __init__(self, channel=20, volume_level=3, on=True, brand='Brand X'):
+    def __init__(self, channel=20, volume_level=3, on=False, brand='Brand X'):
         self.channel = channel
         self.volume_level = volume_level
         self.on = on
