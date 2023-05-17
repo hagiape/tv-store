283dba588fec5625559b7d2226594d68a8dc77de
hagiape <g_chrisf@protonmail.com>
2023-05-17 19:48:29 +0800

    implemented the said methods for simpler syntax

--- TV.py
+++ TV.py
@@ -24,17 +24,15 @@ class TV:
     def get_channel(self):
         return self.channel
     def set_channel(self, channel):
-        if channel > 7 or channel < 1:
-            print('You can only pick between level 1 to 7 of volume level.')
-            self.channel = 20
+        if channel > 120 or channel < 1:
+            self.channel_limit()
         else:
             self.channel = channel
     def get_volume(self):
         return self.volume_level
     def set_volume(self, volume_level):
         if volume_level > 7 or volume_level < 1:
-            print('You can only pick between level 1 to 7 of volume level.')
-            self.volume_level = 3
+            self.volume_level_limit()
         else:
             self.volume_level = volume_level
     def channel_up(self):
