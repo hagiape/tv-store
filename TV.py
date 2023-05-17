70bb44fd47b8a67d53818d3cbe7072361cc9d9b3
hagiape <g_chrisf@protonmail.com>
2023-05-17 17:49:34 +0800

    added conditionals for setting volume and channel ranges for each TV

--- TV.py
+++ TV.py
@@ -1,5 +1,5 @@
 class TV:
-    def __init__(self, name, channel=20, volume_level=3, on=False, brand='Brand X'):
+    def __init__(self, name='TV', channel=20, volume_level=3, on=False, brand='Brand X'):
         self.name = name
         self.channel = channel
         self.volume_level = volume_level
@@ -12,19 +12,27 @@ class TV:
     def get_channel(self):
         return self.channel
     def set_channel(self, channel):
-        self.channel = channel
+        if channel > 7 or channel < 1:
+            print('You can only pick between level 1 to 7 of volume level.')
+            self.channel = 20
+        else:
+            self.channel = channel
     def get_volume(self):
         return self.volume_level
     def set_volume(self, volume_level):
-        self.volume_level = volume_level
+        if volume_level > 7 or volume_level < 1:
+            print('You can only pick between level 1 to 7 of volume level.')
+            self.volume_level = 3
+        else:
+            self.volume_level = volume_level
     def channel_up(self):
-        self.channel+=1
+        self.channel += 1
     def channel_down(self):
-        self.channel-=1
+        self.channel -= 1
     def volume_up(self):
-        self.channel+=1
+        self.channel += 1
     def volume_down(self):
-        self.channel-=1
+        self.channel -=1 
     def display(self):
         print(self.name + "'s channel is " + str(self.channel) + ' and volume level is ' + str(self.volume_level))
         if self.brand == 'Brand X':
